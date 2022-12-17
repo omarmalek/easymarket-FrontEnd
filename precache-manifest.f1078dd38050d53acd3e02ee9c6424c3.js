43cb530cca6a6293e0e8a8b3587a3c3cc2453a89
omarmalek <omar.malek@hotmail.com>
2022-12-17 16:21:38 +0200

    Updates

---
+++ precache-manifest.f1078dd38050d53acd3e02ee9c6424c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "029d1315509285eff2fc",
+    "url": "/easymarket/static/css/main.cae84f7d.chunk.css"
+  },
+  {
+    "revision": "029d1315509285eff2fc",
+    "url": "/easymarket/static/js/main.029d1315.chunk.js"
+  },
+  {
+    "revision": "ef9beaea48e011a4b626",
+    "url": "/easymarket/static/js/1.ef9beaea.chunk.js"
+  },
+  {
+    "revision": "f176d23496ec4e1d8df7",
+    "url": "/easymarket/static/js/runtime~main.f176d234.js"
+  },
+  {
+    "revision": "206f757ca7ca38aba089b8cbd945f534",
+    "url": "/easymarket/static/media/shopping-cart-bag-logo-260nw-small.206f757c.png"
+  },
+  {
+    "revision": "872edcc94dd81092e7c740b4ebd844ae",
+    "url": "/easymarket/static/media/red-simple-shopping-cart-icon-12.872edcc9.png"
+  },
+  {
+    "revision": "6ab8806765e687dde46a85012015fbc8",
+    "url": "/easymarket/static/media/cart.6ab88067.png"
+  },
+  {
+    "revision": "a9c23cc37a4854e4a485680b4a3681c9",
+    "url": "/easymarket/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/easymarket/precache-manifest.f1078dd38050d53acd3e02ee9c6424c3.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/easymarket/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
